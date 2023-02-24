fc44410bef37ca406e71ac253cb6c8805ddfd764
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-24 13:42:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-import ChildComponent from "./ChildComponent";
-
+import React , {useState,useEffect} from 'react'
+import '../styles/App.css';
+import ChildComponent from './ChildComponent';
 const App = () => {
+ //code here
+ const [input, setInput] = useState('');
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+ 
   return (
     <div id="main">
-      <ChildComponent />
+      <ChildComponent> 
+       <input id='input' onChange={handleInputChange} />
+      </ChildComponent>
+      <p id='output'>{Number(input) * Number(input)}</p>
     </div>
   );
 };
+
 
 export default App;
