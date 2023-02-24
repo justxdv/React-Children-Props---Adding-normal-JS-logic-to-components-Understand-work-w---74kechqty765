29895bdfe1891021772383a1a89e444ed6536f5c
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-24 13:36:04 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState, Children } from "react";
 
-const ChildComponent = () => {
-//code here
+const ChildComponent = ({ children }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [outputValue, setOutputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setOutputValue(value * value);
+  };
+
   return (
     <div id="child">
-      Find The Square <br/>
-     
+      {Children.only(children)}
+      <input type="number" id="input" value={inputValue} onChange={handleInputChange} />
+      <p id="output">{outputValue}</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default ChildComponent;
