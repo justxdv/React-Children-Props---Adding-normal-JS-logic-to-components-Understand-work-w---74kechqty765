d2a0faf5b58e7a91ab811366375a2f2b437dac4f
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-24 13:37:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
-import React , {useState,useEffect} from 'react'
-import '../styles/App.css';
-import ChildComponent from './ChildComponent';
-const App = () => {
- //code here
+import React from "react";
+import ChildComponent from "./ChildComponent";
 
+const App = () => {
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
+      <ChildComponent />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
